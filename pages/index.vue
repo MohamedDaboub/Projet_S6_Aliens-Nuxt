79cafd5d3dff49a13e92101e4e2df19aef73b222
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2024-02-02 09:54:26 +0100

    affiche les contnue de la page dans le nuxt

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, expedita blanditiis. Reprehenderit beatae consectetur impedit at. Voluptate distinctio sapiente minus, asperiores, reiciendis facilis maiores, ipsum ratione esse harum soluta animi!</p>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
