27ba2c5d56a29c1a86aa734afe32515c45004eda
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2024-02-10 15:04:38 +0100

    Modifé le header

--- components/Header.vue
+++ components/Header.vue
@@ -1,64 +1,60 @@
 <template>
-<div class="">
-  <header class="gradient-marron-f sticky top-0">
-    <div class="flex flex-wrap justify-between items-center p-2">
-      <RouterLink to="/" class="flex items-center">
-        <img src='/img/Logo.png' class=" w-24 p-2" alt="Logo du site">
-      </RouterLink>
-      <nav class="hidden md:flex items-center">
-        <ul class="flex space-x-2">
-          <li>
-            <router-link to="/" class="text-white   focus:ring-4 focus:ring-primary-300 font-bold rounded-lg text-base px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none" aria-current="page">Home</router-link>
-          </li>
-          <li>
-            <router-link to="/aliens" class="text-white   focus:ring-4 focus:ring-primary-300 font-bold rounded-lg text-base px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none">Aliens</router-link>
-          </li>
-          <li>
-            <router-link to="/aliens" class="text-white   focus:ring-4 focus:ring-primary-300 font-bold rounded-lg text-base px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none">Aliens</router-link>
-          </li>
-          <li>
-            <router-link to="/aliens" class="text-white   focus:ring-4 focus:ring-primary-300 font-bold rounded-lg text-base px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none">Aliens</router-link>
-          </li>
-        </ul>
-      </nav>
-      <div class="flex md:hidden items-center lg:order-2">
-        <button
-          v-directive:click-outside="closeMobileMenu"
-          data-collapse-toggle="mobile-menu"
-          type="button"
-          class=""
-          aria-controls="mobile-menu"
-          aria-expanded="false"
-          @click="toggleMobileMenu"
-        >
-          <span class="sr-only">Open main menu</span>
-          <svg class="w-6 h-6" fill="#fff" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>
-          </svg>
-          <svg v-if="menuOuvert" class="hidden w-6 h-6" fill="#fff" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-          </svg>
-        </button>
-      </div>
+<div class=" sticky top-0 z-10">
+      <header class=" gradient-marron-f  ">
+        <div class="flex flex-wrap justify-between items-center p-2">
+          <RouterLink to="/" class="flex items-center">
+            <img src='/img/Space_Logo.svg' class=" w-20 p-2" alt="Logo du site">
+          </RouterLink>
+          <nav class="hidden md:flex items-center">
+            <ul class="flex space-x-2">
+              <li class="my-5">
+                  <router-link to="/" class="my-3 text-white   focus:ring-4 focus:ring-primary-300 font-bold rounded-lg text-base px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none">Home</router-link>
+
+            </li>
+            <li class="my-5">
+                  <router-link to="/aliens" class=" my-3 text-white   focus:ring-4 focus:ring-primary-300 font-bold rounded-lg text-base px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none">Aliens</router-link>
+
+            </li>
+            <li class="my-5">
+                  <router-link to="/Contact" class=" my-3 text-white   focus:ring-4 focus:ring-primary-300 font-bold rounded-lg text-base px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none">Contact</router-link>
+            </li>
+            </ul>
+          </nav>
+          <div class="flex md:hidden items-center lg:order-2">
+            <i
+              v-directive:click-outside="closeMobileMenu"
+              data-collapse-toggle="mobile-menu"
+              type="button"
+              class=""
+              aria-controls="mobile-menu"
+              aria-expanded="false"
+              @click="toggleMobileMenu"
+            >
+              <span class="sr-only">Open main menu</span>
+              <svg class="w-6 h-6" fill="#fff" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>
+              </svg>
+              <svg v-if="menuOuvert" class="hidden w-6 h-6" fill="#fff" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+              </svg>
+            </i>
+          </div>
+        </div>
+        <nav v-if="menuOuvert" class="md:hidden   w-full h-full border-gray-200 px-4 py-2.5 transition-transform transform  ">
+          <ul class="flex flex-col mt-4 z-10 font-medium justify-center text-white text-center my-10 bg-dark-blue">
+            <li class="my-5">
+                  <router-link to="/" class="text-xs my-3 nav-link">Home</router-link>
+             </li>
+            <li class="my-5">
+                  <router-link to="/aliens" class="text-xs my-3 nav-link">Aliens</router-link>
+            </li>
+            <li class="my-5">
+                  <router-link to="/Contact" class="text-xs my-3 nav-link">Contact</router-link>
+           </li>
+          </ul>
+        </nav>
+      </header>
     </div>
-  </header>
-  <nav v-if="menuOuvert" class="md:hidden z-10  w-full h-full border-gray-200 px-4 py-2.5 transition-transform transform">
-    <ul class="flex flex-col mt-4 font-medium justify-center text-white text-center my-10 bg-dark-blue">
-      <li class="my-5">
-        <nuxt-link to="/" class="nav-link" aria-current="page">Home</nuxt-link>
-      </li>
-      <li class="my-5">
-        <router-link to="/aliens" class="nav-link">Aliens</router-link>
-      </li>
-      <li class="my-5">
-        <router-link to="/aliens" class="nav-link">Aliens</router-link>
-      </li>
-      <li class="my-5">
-        <router-link to="/aliens" class="nav-link">Aliens</router-link>
-      </li>
-    </ul>
-  </nav>
-</div>
 </template>
 
 <script setup>
